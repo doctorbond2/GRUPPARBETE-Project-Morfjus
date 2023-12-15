deb415038055b9dd52c701828ac71d30601fe898
David Pensjö <David.pensjo@gmail.com>
2023-12-15 14:06:03 +0100

    ??

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route } from "react-router";
 import Friends from "./pages/Friends";
