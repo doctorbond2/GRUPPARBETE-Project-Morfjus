63f7f56a1b52c5de34723196ab358f9bfb80f78b
doctorbond2 <michael.rostlund@hotmail.com>
2023-12-22 18:28:55 +0100

    habit small changes

--- src/components/habitsComp/Habit.jsx
+++ src/components/habitsComp/Habit.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
 
 const Habit = ({
   habitItem,
@@ -60,41 +60,53 @@ const Habit = ({
     <>
       <Card>
         <Card.Header>
-          <h2>{title}</h2>
+          <h3>{title}</h3>
         </Card.Header>
         <Card.Body>
-          <h5>
-            Activity Streak:{" "}
-            <small className={streak > 5 ? "habits-activity-streak-color" : ""}>
-              {streak}
-            </small>
-            {""} Days in a row
-          </h5>
-          <Button
-            onClick={(e) => {
-              handleActiveStreak(e);
-            }}
-            variant={"success"}
-            value="inc"
-            className="bi bi-arrow-up"
-          ></Button>
-          <Button
-            onClick={(e) => {
-              handleActiveStreak(e);
-            }}
-            variant={"warning"}
-            value="dec"
-            className="bi bi-arrow-down"
-          ></Button>
-          <Button
-            onClick={(e) => {
-              handleActiveStreak(e);
-            }}
-            value="reset"
-          >
-            Start over
-          </Button>
-          <h6>Priority: {str}</h6>
+          <Row>
+            <Col>
+              <h5>
+                Activity Streak:{" "}
+                <small
+                  className={streak > 5 ? "habits-activity-streak-color" : ""}
+                >
+                  {streak}
+                </small>
+                {""} Days in a row
+              </h5>
+              <h6>Priority: {str}</h6>
+            </Col>
+            <Col className=" col-2">
+              <div className="d-flex flex-column">
+                <Button
+                  size="sm"
+                  onClick={(e) => {
+                    handleActiveStreak(e);
+                  }}
+                  variant={"success"}
+                  value="inc"
+                  className="bi bi-arrow-up"
+                ></Button>
+                <Button
+                  onClick={(e) => {
+                    handleActiveStreak(e);
+                  }}
+                  variant={"warning"}
+                  value="dec"
+                  className="bi bi-arrow-down"
+                ></Button>
+                <Button
+                  size="sm"
+                  onClick={(e) => {
+                    handleActiveStreak(e);
+                  }}
+                  value="reset"
+                >
+                  Start over
+                </Button>
+              </div>
+            </Col>
+          </Row>
         </Card.Body>
       </Card>
     </>
