abb7f9b7eab5877d94a3b7adeba47fc33405a4c8
DavidPensjo <david.pensjo@gmail.com>
2023-12-22 14:41:09 +0100

    added initialHabits

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,11 @@ import {
 } from "./pages";
 import initialTasks from "./mockData/initialTasks";
 import initialFriends from "./mockData/initialFriends";
-
-const defaultHabits = [
-  { title: "Learning guitar", streak: 2, prio: { str: "Low", tier: 1 } },
-  { title: "Reading a book", streak: 7, prio: { str: "High", tier: 3 } },
-  {
-    title: "Daily meditation",
-    streak: 12,
-    prio: { str: "High", tier: 3 },
-  },
-  {
-    title: "Morning jogging",
-    streak: 3,
-    prio: { str: "Medium", tier: 2 },
-  },
-  {
-    title: "Cook a new recipe",
-    streak: 1,
-    prio: { str: "Low", tier: 1 },
-  },
-];
+import initialHabits from "./mockData/initialHabits";
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
-  const [habits, setHabits] = useState(defaultHabits);
+  const [habits, setHabits] = useState(initialHabits);
   const [friends, setFriends] = useState(initialFriends);
   return (
     <>
---
+++ src/mockData/initialHabits.jsx
@@ -0,0 +1,22 @@
+const initialHabits = [
+    { title: "Learning guitar", streak: 2, prio: { str: "Low", tier: 1 } },
+    { title: "Reading a book", streak: 7, prio: { str: "High", tier: 3 } },
+    {
+      title: "Daily meditation",
+      streak: 12,
+      prio: { str: "High", tier: 3 },
+    },
+    {
+      title: "Morning jogging",
+      streak: 3,
+      prio: { str: "Medium", tier: 2 },
+    },
+    {
+      title: "Cook a new recipe",
+      streak: 1,
+      prio: { str: "Low", tier: 1 },
+    },
+  ];
+  
+  export default initialHabits;
+  
