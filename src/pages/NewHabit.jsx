9c8ea90c5a709dbf4dc3215a939072b9e2d98755
doctorbond2 <michael.rostlund@hotmail.com>
2023-12-22 14:18:52 +0100

    almost done with habitInput

--- src/pages/NewHabit.jsx
+++ src/pages/NewHabit.jsx
@@ -2,11 +2,32 @@ import React from "react";
 import { useState } from "react";
 import HabitInput from "../components/habitsComp/HabitInput";
 const NewHabit = ({ habits, setHabits }) => {
+  const [newHabit, setNewHabit] = useState();
   const handleSubmit = (e) => {
     console.log(e.target.value);
   };
-  const handleChange = (e) => {
-    console.log(e.target.value);
+  const handleChange = (e, targetProperty) => {
+    const { value } = e.target;
+    if (targetProperty === "prio" && value !== "") {
+      switch (value) {
+        case "High": {
+          const updatedHabit = { ...newHabit, prio: { str: value, tier: 1 } };
+          break;
+        }
+        case "Medium": {
+          const updatedHabit = { ...newHabit, prio: { str: value, tier: 2 } };
+          break;
+        }
+        case "Low": {
+          const updatedHabit = { ...newHabit, prio: { str: value, tier: 3 } };
+          break;
+        }
+      }
+    } else if (value !== "") {
+      const updatedHabit = { ...newHabit, [targetProperty]: value };
+      setNewHabit(updatedHabit);
+      console.log(newHabit);
+    }
   };
   return (
     <>
