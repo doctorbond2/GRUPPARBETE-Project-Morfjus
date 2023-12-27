d3fcb304f393201e30ca6e5eab5c7d3f30d40f6b
pinsamt <felix.vallejo99v@gmail.com>
2023-12-27 18:52:27 +0100

    Li Tags added

--- src/components/compFriends/FriendsList.jsx
+++ src/components/compFriends/FriendsList.jsx
@@ -10,30 +10,35 @@ const FriendsList = ({ data }) => {
 
   return (
     <>
-      <Card className="d-inline-flex m-2 comp-color" style={{ width: "18rem" }}>
-        <Card.Img
-          className="comp-color"
-          variant="top"
-          src={person.picture.large}
-          alt="picture of profile user"
-        />
-        <Card.Body>
-          <Card.Title>
-            {person.name.title} {person.name.first} {person.name.last}
-          </Card.Title>
-          {showFriends && <Card.Text>Gender: {person.gender}</Card.Text>}
-          {showFriends && <Card.Text>Email: {person.email}</Card.Text>}
-          {showFriends && <Card.Text>Age: {person.dob.age}</Card.Text>}
-          {showFriends && <Card.Text>Born: {person.dob.date}</Card.Text>}
-          <Button
-            onClick={() => {
-              setShowFriends(!showFriends);
-            }}
-          >
-            {!showFriends ? "Show More" : "Show Less"}
-          </Button>
-        </Card.Body>
-      </Card>
+      <li>
+        <Card
+          className="d-inline-flex m-2 comp-color"
+          style={{ width: "18rem" }}
+        >
+          <Card.Img
+            className="comp-color"
+            variant="top"
+            src={person.picture.large}
+            alt="picture of profile user"
+          />
+          <Card.Body>
+            <Card.Title>
+              {person.name.title} {person.name.first} {person.name.last}
+            </Card.Title>
+            {showFriends && <Card.Text>Gender: {person.gender}</Card.Text>}
+            {showFriends && <Card.Text>Email: {person.email}</Card.Text>}
+            {showFriends && <Card.Text>Age: {person.dob.age}</Card.Text>}
+            {showFriends && <Card.Text>Born: {person.dob.date}</Card.Text>}
+            <Button
+              onClick={() => {
+                setShowFriends(!showFriends);
+              }}
+            >
+              {!showFriends ? "Show More" : "Show Less"}
+            </Button>
+          </Card.Body>
+        </Card>
+      </li>
     </>
   );
 };
